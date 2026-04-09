5a1bcabd786a41c80cf503aeb4e34f6dc4f6ef62
Sojanya Sharma <sojanya7@gmail.com>
2026-04-09 23:13:06 +0530

    database connect

--- routes/pool.js
+++ routes/pool.js
@@ -1,19 +1,20 @@
-const mysql=require('mysql2')
- var pool=mysql.createConnection({
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
- })
+  port: process.env.DB_PORT
+});
 
-pool.connect((err) => {
+connection.connect((err) => {
   if (err) {
-    console.log("DB Error:", err);
+    console.log("❌ DB Error:", err);
   } else {
-    console.log("MySQL Connected ");
+    console.log("✅ DB Connected");
   }
 });
 
- module.exports=pool
- 
+module.exports = connection;
