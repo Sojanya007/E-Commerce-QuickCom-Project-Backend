71eaf27d602508f5b8081c8ac04e1bc79876f4e8
Sojanya Sharma <sojanya7@gmail.com>
2026-04-18 17:55:58 +0530

    fix code

--- routes/pool.js
+++ routes/pool.js
@@ -6,16 +6,14 @@ const pool = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: Number(process.env.DB_PORT) || 3306
 });
 
 pool.query((err) => {
   if (err) {
-    console.log("DB Error:", err.code);
-  }
-  else
-  {
-    console.log("DB Connected");
+    console.log("❌ DB ERROR:", err);
+  } else {
+    console.log("✅ DB WORKING");
   }
 });
 
