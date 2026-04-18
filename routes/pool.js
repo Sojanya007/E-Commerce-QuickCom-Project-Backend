502b9eb58756bee75793931c0c6940393b7f9bb1
Sojanya Sharma <sojanya7@gmail.com>
2026-04-18 18:42:41 +0530

    fix code

--- routes/pool.js
+++ routes/pool.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+/*require("dotenv").config();
 const mysql = require("mysql2");
 
 const pool = mysql.createConnection({
@@ -14,6 +14,31 @@ pool.query("SELECT 1",(err, result) => {
     console.log("❌ DB ERROR:", err);
   } else {
     console.log("✅ DB WORKING");
+  }
+});
+
+module.exports = pool;
+*/
+require("dotenv").config();
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT) || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+// ✅ TEST QUERY
+pool.query("SELECT 1", (err, result) => {
+  if (err) {
+    console.log("❌ DB ERROR:", err);
+  } else {
+    console.log("✅ DB WORKING (POOL)");
   }
 });
 
