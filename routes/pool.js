1d5d6a07fdb6bac04bf9bba9037f32820bdf0f47
Sojanya Sharma <sojanya7@gmail.com>
2026-04-18 17:37:26 +0530

    fix changes

--- routes/pool.js
+++ routes/pool.js
@@ -1,19 +1,12 @@
 require("dotenv").config();
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
+const pool = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-   ssl: {
-    rejectUnauthorized: false
-  },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit:0
-
+  port: process.env.DB_PORT
 });
 
 pool.query((err) => {
